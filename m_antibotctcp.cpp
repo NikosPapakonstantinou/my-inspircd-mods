414c8c709ab1237307aced4c86d6e9259834c6a8
Nikos-ioh <url.euro@gmail.com>
2015-07-12 02:37:53 +0300

    update code style

--- m_antibotctcp.cpp
+++ m_antibotctcp.cpp
@@ -43,79 +43,81 @@ class ModuleAntiBotCTCP : public Module
         
         void init()
         {
-                ServerInstance->Modules->AddService(ext);
-                Implementation eventlist[] = { I_OnUserRegister, I_OnPreCommand, I_OnCheckReady, I_OnRehash };
-                ServerInstance->Modules->Attach(eventlist, this, sizeof(eventlist)/sizeof(Implementation));
-                OnRehash(NULL);
+			ServerInstance->Modules->AddService(ext);
+			Implementation eventlist[] = { I_OnUserRegister, I_OnPreCommand, I_OnCheckReady, I_OnRehash };
+			ServerInstance->Modules->Attach(eventlist, this, sizeof(eventlist)/sizeof(Implementation));
+			OnRehash(NULL);
         }
         
         Version GetVersion()
         {
-                return Version("Blocks clients not replying to CTCP.");
+			return Version("Blocks clients not replying to CTCP.");
         }
         
         void OnRehash(User* user)
         {
-                ConfigTag* tag = ServerInstance->Config->ConfValue("antibotctcp");
-                quitmsg = tag->getBool("quitmsg", true);
-                msgonreply = tag->getBool("msgonreply", true);
-                ctcp = tag->getString("ctcp");
-                link = tag->getString("link");
-                if(ctcp.empty())
-                {
-                        ServerInstance->SNO->WriteGlobalSno('a', "m_antibotctcp: Invalid ctcp value in config: %s", ctcp.c_str());
-                        ServerInstance->Logs->Log("m_antibotctcp",DEFAULT, "m_antibotctcp: Invalid ctcp value in config: %s", ctcp.c_str());
-                }
-                else
-                {
-                        tmp = "\001" + ctcp      + " ";
-                }
+			ConfigTag* tag = ServerInstance->Config->ConfValue("antibotctcp");
+			quitmsg = tag->getBool("quitmsg", true);
+			msgonreply = tag->getBool("msgonreply", true);
+			ctcp = tag->getString("ctcp");
+			link = tag->getString("link");
+			if(ctcp.empty())
+			{
+				ServerInstance->SNO->WriteGlobalSno('a', "m_antibotctcp: Invalid ctcp value in config: %s", ctcp.c_str());
+				ServerInstance->Logs->Log("m_antibotctcp",DEFAULT, "m_antibotctcp: Invalid ctcp value in config: %s", ctcp.c_str());
+			}
+			else
+			{
+				tmp = "\001" + ctcp      + " ";
+			}
         }
         
         ModResult OnUserRegister(LocalUser* user)
         {
-                ConfigTag* tag = user->MyClass->config;
-                if (tag->getBool("antibotctcp", true))
-                {
-                        user->WriteNumeric(931, "%s :Malicious or potentially unwanted softwares are not WELCOME here!", user->nick.c_str());
-                        user->WriteServ("PRIVMSG %s :\001%s\001", user->nick.c_str(), ctcp.c_str());
-                        ext.set(user, 1);
-                        return MOD_RES_PASSTHRU;
-                }
-                else
-                }
-			return MOD_RES_PASSTHRU;
-                }
+			ConfigTag* tag = user->MyClass->config;
+			if (tag->getBool("antibotctcp", true))
+			{
+				user->WriteNumeric(931, "%s :Malicious or potentially unwanted softwares are not WELCOME here!", user->nick.c_str());
+				user->WriteServ("PRIVMSG %s :\001%s\001", user->nick.c_str(), ctcp.c_str());
+				ext.set(user, 1);
+				return MOD_RES_PASSTHRU;
+			}
+			else
+			}
+				return MOD_RES_PASSTHRU;
+			}
         }
         
         ModResult OnPreCommand(std::string &command, std::vector<std::string> &parameters, LocalUser* user, bool validated, const std::string &original_line)
         {
-                if (command == "NOTICE" && !validated && parameters.size() > 1 && ext.get(user))
-                {
-                        if (parameters[1].compare(0, tmp.length(), tmp) == 0)
-                        {
-                                ext.set(user, 0);
-                                if (msgonreply)
-                                        user->WriteServ("NOTICE " + user->nick + " :*** Howdy buddy,you are authorized to use this server!");
-                                return MOD_RES_DENY;
-                        }
+			if (command == "NOTICE" && !validated && parameters.size() > 1 && ext.get(user))
+			{
+				if (parameters[1].compare(0, tmp.length(), tmp) == 0)
+				{
+					ext.set(user, 0);
+					if (msgonreply)
+					{
+						user->WriteServ("NOTICE " + user->nick + " :*** Howdy buddy,you are authorized to use this server!");
+					}
+					return MOD_RES_DENY;
+				}
+			}
+			return MOD_RES_PASSTHRU;
 		}
-                return MOD_RES_PASSTHRU;
-        }
         
         ModResult OnCheckReady(LocalUser* user)
         {
-                if (ext.get(user))
-		{
-			if (quitmsg)
+			if (ext.get(user))
 			{
-				std::string qmsg;
-				qmsg = "If you are having problems connecting to this server, please get a better CLIENT or visit " + link + " for more info.";
-				ServerInstance->Users->QuitUser(user, qmsg);
+				if (quitmsg)
+				{
+					std::string qmsg;
+					qmsg = "If you are having problems connecting to this server, please get a better CLIENT or visit " + link + " for more info.";
+					ServerInstance->Users->QuitUser(user, qmsg);
+				}
+				return MOD_RES_DENY;
 			}
-			return MOD_RES_DENY;
-		}
-                return MOD_RES_PASSTHRU;
+			return MOD_RES_PASSTHRU;
         }
 };
 MODULE_INIT(ModuleAntiBotCTCP)
